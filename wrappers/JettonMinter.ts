2dc7703862c877acb04bb571a1c0f67a795168f8
Gusarich <danielsedovzzz@gmail.com>
2023-03-23 21:08:32 +0300

    standard jetton wrappers

--- wrappers/JettonMinter.ts
+++ wrappers/JettonMinter.ts
@@ -1,9 +1,18 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
 
-export type JettonMinterConfig = {};
+export type JettonMinterConfig = {
+    admin: Address;
+    content: Cell;
+    walletCode: Cell;
+};
 
 export function jettonMinterConfigToCell(config: JettonMinterConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeCoins(0)
+        .storeAddress(config.admin)
+        .storeRef(config.content)
+        .storeRef(config.walletCode)
+        .endCell();
 }
 
 export class JettonMinter implements Contract {
@@ -24,6 +33,30 @@ export class JettonMinter implements Contract {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
+        });
+    }
+
+    async sendMint(provider: ContractProvider, via: Sender, value: bigint, recipient: Address, amount: bigint) {
+        await provider.internal(via, {
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(0x1674b0a0, 32)
+                .storeUint(0, 64)
+                .storeAddress(recipient)
+                .storeCoins(amount)
+                .storeRef(
+                    beginCell()
+                        .storeUint(0x178d4519, 32)
+                        .storeUint(0, 64)
+                        .storeCoins(amount)
+                        .storeAddress(this.address)
+                        .storeAddress(this.address)
+                        .storeCoins(0)
+                        .storeUint(0, 1)
+                        .endCell()
+                )
+                .endCell(),
+            value: value,
         });
     }
 }
--- wrappers/JettonWallet.ts
+++ wrappers/JettonWallet.ts
@@ -1,9 +1,18 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
 
-export type JettonWalletConfig = {};
+export type JettonWalletConfig = {
+    owner: Address;
+    minter: Address;
+    walletCode: Cell;
+};
 
 export function jettonWalletConfigToCell(config: JettonWalletConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeCoins(0)
+        .storeAddress(config.owner)
+        .storeAddress(config.minter)
+        .storeRef(config.walletCode)
+        .endCell();
 }
 
 export class JettonWallet implements Contract {
@@ -25,5 +34,38 @@ export class JettonWallet implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async sendTransfer(
+        provider: ContractProvider,
+        via: Sender,
+        value: bigint,
+        forwardValue: bigint,
+        recipient: Address,
+        amount: bigint
+    ) {
+        await provider.internal(via, {
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(0x0f8a7ea5, 32)
+                .storeUint(0, 64)
+                .storeCoins(amount)
+                .storeAddress(recipient)
+                .storeAddress(via.address)
+                .storeUint(0, 1)
+                .storeCoins(forwardValue)
+                .storeUint(0, 1)
+                .endCell(),
+            value: value + forwardValue,
+        });
+    }
+
+    async getJettonBalance(provider: ContractProvider) {
+        let state = await provider.getState();
+        if (state.state.type !== 'active') {
+            return 0n;
+        }
+        let res = await provider.get('get_wallet_data', []);
+        return res.stack.readBigNumber();
     }
 }
